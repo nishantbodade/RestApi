fba67bc9bd63fa9105098296e2177e4d2f2624ac
Nishant Bodade <nishantbodade@gmail.com>
2022-03-04 13:01:37 +0530

    @ParhVariable demo

--- spring-in-10-steps/spring-in-10-steps/src/main/java/com/in28minutes/springin10steps/controller/HelloWordController.java
+++ spring-in-10-steps/spring-in-10-steps/src/main/java/com/in28minutes/springin10steps/controller/HelloWordController.java
@@ -1,10 +1,7 @@
 package com.in28minutes.springin10steps.controller;
 
 import com.in28minutes.springin10steps.model.HelloWorldBean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWordController {
@@ -12,5 +9,10 @@ public class HelloWordController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/helloWorld/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
