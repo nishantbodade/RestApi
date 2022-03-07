0cda036b75112ccb4015d6ca01486b50a0822ea8
Nishant Bodade <nishantbodade@gmail.com>
2022-03-07 11:36:24 +0530

    generalize exception handlling for all resources

--- spring-in-10-steps/spring-in-10-steps/src/main/java/com/in28minutes/springin10steps/exception/UserNotFoundException.java
+++ spring-in-10-steps/spring-in-10-steps/src/main/java/com/in28minutes/springin10steps/exception/UserNotFoundException.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String message) {
         super(message);
-        System.out.println(message);
+
 
 
 
